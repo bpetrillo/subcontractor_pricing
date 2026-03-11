9903debe72fba020b99d26e1630ab80c78052734
bpetrillo <bpetrillo@sunbeltutilities.com>
2026-03-11 15:34:10 -0400

    Create Main.jsx

---
+++ Main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import SubPricingTool from "./SubPricingTool";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <SubPricingTool />
+  </StrictMode>
+);
